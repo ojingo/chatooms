a49f39539b7060491e92abdcd9e920b09986e20b
TJ <tj@ojingolabs.com>
2013-07-28 15:31:02 +0400

    serveStatic function ( check it! )

--- server.js
+++ server.js
@@ -25,3 +25,23 @@ function sendFile(response, filePath, fileContents) {
     response.end(fileContents);
 }
 
+function serveStatic(response, cache, absPath) {
+    if(cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists) {
+            if(exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if(err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
+        });
+    }
+}
