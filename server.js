9c065e66f09888433a967c2054427df31453557c
TJ <tj@ojingolabs.com>
2013-07-28 15:52:42 +0400

    http server create function

--- server.js
+++ server.js
@@ -45,3 +45,18 @@ function serveStatic(response, cache, absPath) {
         });
     }
 }
+
+var server = http.createServer(function(request, response) {
+    var filePath = false;
+
+    if (request.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+
+    var absPath = './' + filePath;
+    serveStatic(response, cache, absPath);
+});
+
+
